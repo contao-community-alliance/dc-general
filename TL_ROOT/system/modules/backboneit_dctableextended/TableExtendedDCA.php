33cde514d16c707b6d21c28dd0ec4e03289d3093
Oliver Hoff <oliver@hofff.com>
2011-09-08 01:55:44 +0000

    see changelog 0.2.0 stable

--- TL_ROOT/system/modules/backboneit_dctableextended/TableExtendedDCA.php
+++ TL_ROOT/system/modules/backboneit_dctableextended/TableExtendedDCA.php
@@ -3,9 +3,20 @@
 class TableExtendedDCA extends Backend {
 	
 	protected $arrExcludes;
+
+	protected function __construct() {
+		parent::__construct();
+		$this->arrExcludes = array_flip(deserialize($GLOBALS['TL_CONFIG']['backboneit_dctableextended_excludes'], true));
+	}
 	
 	public function isExtendedTable($strTable) {
 		return !isset($this->arrExcludes[$strTable]);
+	}
+	
+	public function setup($strTable) {
+		$strDC = &$GLOBALS['TL_DCA'][$strTable]['config']['dataContainer'];
+		if($strDC == 'Table' && $this->isExtendedTable($strTable))
+			$strDC = 'TableExtended';
 	}
 	
 	public function getExclusionOptions() {
@@ -24,37 +35,6 @@ class TableExtendedDCA extends Backend {
 		ksort($arrOptions);
 		
 		return $arrOptions;
-	}
-	
-	public function setup($strTable) {
-		if(!$this->isExtendedTable($strTable))
-			return;
-			
-		$strDC = &$GLOBALS['TL_DCA'][$strTable]['config']['dataContainer'];
-		if($strDC == 'Table')
-			$strDC = 'TableExtended';
-	}
-
-	public function fixPagePicker($strTable) {
-		if($strTable != 'tl_content')
-			return;
-			
-		foreach($GLOBALS['TL_DCA']['tl_content']['fields']['url']['wizard'] as &$arrCallback) {
-			if($arrCallback[0] == 'tl_content' && $arrCallback[1] == 'pagePicker') {
-				$arrCallback[0] = 'TableExtendedDCA';
-				break;
-			}
-		}
-	}
-	
-	public function pagePicker(DataContainer $dc) {
-		$strField = 'ctrl_' . $dc->inputName;
-		return ' ' . $this->generateImage('pickpage.gif', $GLOBALS['TL_LANG']['MSC']['pagepicker'], 'style="vertical-align:top; cursor:pointer;" onclick="Backend.pickPage(\'' . $strField . '\')"');
-	}
-	
-	protected function __construct() {
-		parent::__construct();
-		$this->arrExcludes = array_flip(deserialize($GLOBALS['TL_CONFIG']['backboneit_dctableextended_excludes'], true));
 	}
 	
 	private static $objInstance;
--- TL_ROOT/system/modules/backboneit_dctableextended/config/config.php
+++ TL_ROOT/system/modules/backboneit_dctableextended/config/config.php
@@ -1,8 +1,6 @@
 <?php
 
-if(!isset($GLOBALS['TL_CONFIG']['backboneit_dctableextended_excludes']))
-	$GLOBALS['TL_CONFIG']['backboneit_dctableextended_excludes'] = array('tl_layout');
+$GLOBALS['TL_CONFIG']['backboneit_dctableextended_excludes'] = array('tl_layout');
 
 $GLOBALS['TL_HOOKS']['loadDataContainer'][] = array('TableExtendedDCA', 'setup');
-$GLOBALS['TL_HOOKS']['loadDataContainer'][] = array('TableExtendedDCA', 'fixPagePicker');
 $GLOBALS['TL_HOOKS']['executePostActions'][] = array('TableExtendedAjax', 'executePostActions');
