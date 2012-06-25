3f03c678d3e0ec8850c862f9df0a4a091ca68113
Stefan heimes <stefan_heimes@hotmail.com>
2012-06-25 18:06:25 +0200

    Add new classes for callbacks

---
+++ TL_ROOT/system/modules/generalDriver/InterfaceGeneralCallback.php
@@ -0,0 +1,134 @@
+<?php
+
+if (!defined('TL_ROOT'))
+    die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  MEN AT WORK 2012
+ * @package    generalDriver
+ * @license    GNU/LGPL
+ * @filesource
+ */
+interface InterfaceGeneralCallback
+{
+
+    /**
+     * Exectue a callback
+     * 
+     * @param array $varCallbacks
+     * @return array 
+     */
+    public function executeCallbacks($varCallbacks);
+
+    /**
+     * Set the DC
+     * 
+     * @param DC_General $objDC
+     */
+    public function setDC($objDC);
+
+    /**
+     * Call the customer label callback
+     * 
+     * @param InterfaceGeneralModel $objModelRow
+     * @param string $mixedLabel
+     * @param array $args
+     * @return string 
+     */
+    public function labelCallback(InterfaceGeneralModel $objModelRow, $mixedLabel, $args);
+
+    /**
+     * Call the button callback for the regular operations
+     * 
+     * @param InterfaceGeneralModel $objModelRow
+     * @param string $strLabel
+     * @param string $strTitle
+     * @param array $arrAttributes
+     * @param string $strTable
+     * @param array $arrRootIds
+     * @param array $arrChildRecordIds
+     * @param boolean $blnCircularReference
+     * @param string $strPrevious
+     * @param string $strNext
+     * @return string|null 
+     */
+    public function buttonCallback($objModelRow, $strLabel, $strTitle, $arrAttributes, $strTable, $arrRootIds, $arrChildRecordIds, $blnCircularReference, $strPrevious, $strNext);
+
+    /**
+     * Call the button callback for the global operations
+     * 
+     * @param str $strLabel
+     * @param str $strTitle
+     * @param array $arrAttributes
+     * @param string $strTable
+     * @param array $arrRootIds
+     * @return string|null 
+     */
+    public function globalButtonCallback($strLabel, $strTitle, $arrAttributes, $strTable, $arrRootIds);
+
+    /**
+     * Call the options callback for the fields
+     * 
+     * @return array|null 
+     */
+    public function optionsCallback();
+
+    /**
+     * Call the onrestore callback
+     * 
+     * @param int $intID ID of current dataset
+     * @param string $strTable Name of current Table
+     * @param array $arrData Array with all Data
+     * @param int $intVersion Version which was restored
+     */
+    public function onrestoreCallback($intID, $strTable, $arrData, $intVersion);
+
+    /**
+     * Call onload_callback (e.g. to check permissions)
+     * 
+     * @param string $strTable name of current table
+     */
+    public function onloadCallback();
+
+    /**
+     * Call the group callback 
+     * 
+     * @param type $group
+     * @param type $mode
+     * @param type $field
+     * @param InterfaceGeneralModel $objModelRow
+     * @return type 
+     */
+    public function groupCallback($group, $mode, $field, $objModelRow);
+
+    /**
+     * Call the save callback for a widget
+     * 
+     * @param array $arrConfig Configuration of the widget
+     * @param mixed $varNew New Value
+     * @return mixed 
+     */
+    public function saveCallback($arrConfig, $varNew);
+}
+
+?>
