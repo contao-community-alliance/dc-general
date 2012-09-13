e3e89727b6eda25ef3139d279b08584157428abb
Christian Schiffler <c.schiffler@cyberspectrum.de>
2012-09-13 02:54:00 +0200

    fixed typo in constant name in GeneralViewDefault. added optional $intId param to InterfaceGeneralData::isUniqueValue() to allow the model currently in the edit buffe to keep its value.

--- system/modules/generalDriver/InterfaceGeneralData.php
+++ system/modules/generalDriver/InterfaceGeneralData.php
@@ -28,59 +28,59 @@
  */
 interface InterfaceGeneralData
 {
-    
+
     /**
      * Set base config with source and other neccesary prameter
-     * 
+     *
      * @param array $arrConfig
-     * @throws Excpetion 
+     * @throws Excpetion
      */
     public function setBaseConfig(array $arrConfig);
 
     /**
      * Return empty config object
-     * 
+     *
      * @return InterfaceGeneralDataConfig
      */
     public function getEmptyConfig();
-    
+
     /**
      * Fetch an empty single record (new item).
-     * 
+     *
      * @return InterfaceGeneralModel
      */
     public function getEmptyModel();
-    
+
     /**
      * Fetch an empty new collection.
-     * 
+     *
      * @return InterfaceGeneralCollection
      */
-    public function getEmptyCollection();    
-    
+    public function getEmptyCollection();
+
     /**
      * Fetch a single record by id.
-     * 
+     *
      * @param GeneralDataConfigDefault $objConfig
-     * 
+     *
      * @return InterfaceGeneralModel
      */
     public function fetch(GeneralDataConfigDefault $objConfig);
 
     /**
      * Fetch multiple records by ids.
-     * 
+     *
      * @param GeneralDataConfigDefault $objConfig
-     * 
+     *
      * @return InterfaceGeneralCollection
      */
     public function fetchEach(GeneralDataConfigDefault $objConfig);
 
     /**
      * Fetch all records (optional limited).
-     * 
+     *
      * @param GeneralDataConfigDefault $objConfig
-     * 
+     *
      * @return InterfaceGeneralCollection
      */
     public function fetchAll(GeneralDataConfigDefault $objConfig);
@@ -89,7 +89,7 @@ interface InterfaceGeneralData
      * Return the amount of total items.
      *
      * @param GeneralDataConfigDefault $objConfig
-     * 
+     *
      * @return int
      */
     public function getCount(GeneralDataConfigDefault $objConfig);
@@ -112,89 +112,95 @@ interface InterfaceGeneralData
 
     /**
      * Delete an item.
-     * 
+     *
      * @param int|Module Id or the object itself, to delete
      */
     public function delete($item);
 
     /**
      * Save a new Version of a record
-     * 
+     *
      * @param int $intID ID of current record
      * @param string $strVersion Version number
-     * @return void 
+     * @return void
      */
     public function saveVersion(InterfaceGeneralModel $objModel, $strUsername);
 
     /**
      * Return a model based of the version information
-     * 
+     *
      * @param mix $mixID The ID of record
      * @param mix $mixVersion The ID of the Version
-     * 
-     * @return InterfaceGeneralModel 
+     *
+     * @return InterfaceGeneralModel
      */
     public function getVersion($mixID, $mixVersion);
 
     /**
      * Return a list with all versions for this row
-     * 
+     *
      * @param mixed $mixID The ID of record
-     * 
-     * @return InterfaceGeneralCollection 
+     *
+     * @return InterfaceGeneralCollection
      */
     public function getVersions($mixID);
 
     /**
-     * Set a Version as active. 
-     * 
+     * Set a Version as active.
+     *
      * @param mix $mixID The ID of record
      * @param mix $mixVersion The ID of the Version
      */
     public function setVersionActive($mixID, $mixVersion);
-    
+
     /**
      * Return the active version from a record
-     * 
+     *
      * @param mix $mixID The ID of record
-     * 
-     * @return mix Version ID 
+     *
+     * @return mix Version ID
      */
     public function getActiveVersion($mixID);
 
     /**
-     * Reste the fallback field 
-     * 
-     * Documentation: 
+     * Reste the fallback field
+     *
+     * Documentation:
      *      Evaluation - fallback => If true the field can only be assigned once per table.
-     * 
+     *
      * @return void
      */
     public function resetFallback($strField);
 
     /**
      * Check if the value is unique in table
-     * 
-     * Documentation: 
+     *
+	 * @param string $strField the field in which to test.
+	 *
+	 * @param mixed  $varNew   the value about to be saved.
+	 *
+	 * @param int    $intId    the (optional) id of the item currently in scope - pass null for new items.
+	 *
+     * Documentation:
      *      Evaluation - unique => If true the field value cannot be saved if it exists already.
-     * 
+     *
      * @return boolean
      */
-    public function isUniqueValue($strField, $varNew);
-    
+    public function isUniqueValue($strField, $varNew, $intId = null);
+
     /**
      * Check if the value exists in the table
-     * 
-     * @return boolean 
+     *
+     * @return boolean
      */
     public function fieldExists($strField);
-    
+
     /**
      * Check if two models have the same properties
-     * 
+     *
      * @param InterfaceGeneralModel $objModel1
      * @param InterfaceGeneralModel $objModel2
-     * 
+     *
      * return boolean True - If both models are same, false if not
      */
     public function sameModels($objModel1 , $objModel2);
