1d1e40a81bf5d301e08ffa9df7582fb66afbce06
Tristan Lins <tristan.lins@bit3.de>
2013-08-24 20:22:16 +0200

    Rename DcGeneral/Panel/BaseContainer to DcGeneral/Panel/DefaultPanelContainer.

---
+++ system/modules/generalDriver/DcGeneral/Panel/BasePanelContainer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DcGeneral\Panel;
+
+trigger_error('Usage of deprecated '. __FILE__, E_USER_DEPRECATED);
+/**
+ * @deprecated
+ */
+class BaseContainer extends DefaultPanelContainer
+{
+}
