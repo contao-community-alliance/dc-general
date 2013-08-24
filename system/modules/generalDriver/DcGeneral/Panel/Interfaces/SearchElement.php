917f8626aba3a8bcc900d58252969433303b49dc
Tristan Lins <tristan.lins@bit3.de>
2013-08-24 20:12:40 +0200

    Rename DcGeneral/Panel/Interfaces/SearchElement to DcGeneral/Panel/SearchElementInterface.

--- system/modules/generalDriver/DcGeneral/Panel/Interfaces/SearchElement.php
+++ system/modules/generalDriver/DcGeneral/Panel/Interfaces/SearchElement.php
@@ -2,49 +2,12 @@
 
 namespace DcGeneral\Panel\Interfaces;
 
-interface SearchElement extends PanelElementInterface
+use DcGeneral\Panel\SearchElementInterface;
+
+trigger_error('Usage of deprecated '. __FILE__, E_USER_DEPRECATED);
+/**
+ * @deprecated
+ */
+interface SearchElement extends SearchElementInterface
 {
-	/**
-	 * @param string $strProperty The property to allow to search on.
-	 *
-	 * @return SearchElement
-	 */
-	public function addProperty($strProperty);
-
-	/**
-	 * Retrieve the list of properties to allow search on.
-	 *
-	 * @return string[]
-	 */
-	public function getPropertyNames();
-
-	/**
-	 * This activates a property for search.
-	 *
-	 * @param string $strProperty The property to activate search on.
-	 *
-	 * @return SearchElement
-	 */
-	public function setSelectedProperty($strProperty = '');
-
-	/**
-	 * Retrieves the property currently defined to be searched on.
-	 *
-	 * @return string
-	 */
-	public function getSelectedProperty();
-
-	/**
-	 * Set the value to search for.
-	 *
-	 * @param mixed $mixValue The value to filter for.
-	 *
-	 * @return SearchElement
-	 */
-	public function setValue($mixValue = null);
-
-	/**
-	 * @return mixed
-	 */
-	public function getValue();
 }
