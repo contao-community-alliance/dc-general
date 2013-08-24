721ab0e575ced05b1b26f0677b1f98c2baa2d586
Tristan Lins <tristan.lins@bit3.de>
2013-08-24 15:09:06 +0200

    Rename DcGeneral/Data/Config to DcGeneral/Data/DefaultConfig.

--- system/modules/generalDriver/GeneralDataConfigDefault.php
+++ system/modules/generalDriver/GeneralDataConfigDefault.php
@@ -10,6 +10,6 @@
  */
 
 trigger_error('Usage of deprecated '. __FILE__, E_USER_DEPRECATED);
-class GeneralDataConfigDefault extends DcGeneral\Data\Config
+class GeneralDataConfigDefault extends DcGeneral\Data\DefaultConfig
 {
 }
