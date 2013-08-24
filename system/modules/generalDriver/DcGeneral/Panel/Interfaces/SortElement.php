2403bdae8463d41fd7776051251cab8fe222bb31
Tristan Lins <tristan.lins@bit3.de>
2013-08-24 20:14:12 +0200

    Rename DcGeneral/Panel/Interfaces/SortElement to DcGeneral/Panel/SortElementInterface.

--- system/modules/generalDriver/DcGeneral/Panel/Interfaces/SortElement.php
+++ system/modules/generalDriver/DcGeneral/Panel/Interfaces/SortElement.php
@@ -2,63 +2,12 @@
 
 namespace DcGeneral\Panel\Interfaces;
 
-interface SortElement extends PanelElementInterface
+use DcGeneral\Panel\SortElementInterface;
+
+trigger_error('Usage of deprecated '. __FILE__, E_USER_DEPRECATED);
+/**
+ * @deprecated
+ */
+interface SortElement extends SortElementInterface
 {
-	/**
-	 * Set the default flag to use when no flag has been defined for a certain property.
-	 *
-	 * @param int $intFlag The flag to use.
-	 *
-	 * @return SearchElementInterface
-	 */
-	public function setDefaultFlag($intFlag);
-
-	/**
-	 * Get the default flag to use when no flag has been defined for a certain property.
-	 *
-	 * @return int
-	 */
-	public function getDefaultFlag();
-
-	/**
-	 * Add a property for sorting.
-	 *
-	 * @param $strPropertyName
-	 *
-	 * @param $intFlag
-	 *
-	 * @return mixed
-	 */
-	public function addProperty($strPropertyName, $intFlag);
-
-	/**
-	 * Retrieve the list of properties to allow search on.
-	 *
-	 * @return string[]
-	 */
-	public function getPropertyNames();
-
-	/**
-	 * Set the selected property for sorting.
-	 *
-	 * @param $strPropertyName
-	 *
-	 * @return mixed
-	 */
-	public function setSelected($strPropertyName);
-
-	/**
-	 * Return the name of the currently selected property.
-	 *
-	 * @return string
-	 */
-	public function getSelected();
-
-	/**
-	 * Return the flag of the currently selected property.
-	 *
-	 * @return int
-	 */
-	public function getFlag();
-	// TODO: wouln't it be nice to also have a direction setting here instead of only the flag?
 }
