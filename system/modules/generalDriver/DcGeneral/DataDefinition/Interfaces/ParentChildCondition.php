17850b7582c42aaa2b0b2c82db866fc7d7080248
Tristan Lins <tristan.lins@bit3.de>
2013-08-24 16:24:19 +0200

    Rename DcGeneral/DataDefinition/Interfaces/ParentChildCondition to DcGeneral/DataDefinition/ParentChildConditionInterface.

--- system/modules/generalDriver/DcGeneral/DataDefinition/Interfaces/ParentChildCondition.php
+++ system/modules/generalDriver/DcGeneral/DataDefinition/Interfaces/ParentChildCondition.php
@@ -11,53 +11,12 @@
 
 namespace DcGeneral\DataDefinition\Interfaces;
 
-use DcGeneral\Data\ModelInterface;
-use DcGeneral\DataDefinition\ConditionInterface;
+use DcGeneral\DataDefinition\ParentChildConditionInterface;
 
-interface ParentChildCondition extends ConditionInterface
+trigger_error('Usage of deprecated '. __FILE__, E_USER_DEPRECATED);
+/**
+ * @deprecated
+ */
+interface ParentChildCondition extends ParentChildConditionInterface
 {
-	/**
-	 * Get the condition as filter.
-	 *
-	 * @param ModelInterface $objParent The model that shall get used as parent.
-	 *
-	 * @return array
-	 */
-	public function getFilter($objParent);
-
-	/**
-	 * Apply a condition to a child.
-	 *
-	 * @param ModelInterface $objParent The parent object.
-	 *
-	 * @param ModelInterface $objChild The object on which the condition shall be enforced on.
-	 *
-	 * @return void
-	 */
-	public function applyTo($objParent, $objChild);
-
-	/**
-	 * Test if the given parent is indeed a parent of the given child object for this condition.
-	 *
-	 * @param ModelInterface $objParent
-	 *
-	 * @param ModelInterface $objChild
-	 *
-	 * @return bool
-	 */
-	public function matches($objParent, $objChild);
-
-	/**
-	 * Return the name of the source provider.
-	 *
-	 * @return string
-	 */
-	public function getSourceName();
-
-	/**
-	 * Return the name of the destination provider.
-	 *
-	 * @return string
-	 */
-	public function getDestinationName();
 }
