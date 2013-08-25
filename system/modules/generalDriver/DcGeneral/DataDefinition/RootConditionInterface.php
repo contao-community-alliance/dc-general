ae2ff75c533264ffc1aee97e159805ed4cb4bba5
Tristan Lins <tristan.lins@bit3.de>
2013-08-25 13:13:01 +0200

    Add Tristan Lins as author.

--- system/modules/generalDriver/DcGeneral/DataDefinition/RootConditionInterface.php
+++ system/modules/generalDriver/DcGeneral/DataDefinition/RootConditionInterface.php
@@ -4,6 +4,7 @@
  * @package    generalDriver
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
  * @copyright  The MetaModels team.
  * @license    LGPL.
  * @filesource
