1f5ddc70ffbb3498c4b017cede461ef590a8982d
Tristan Lins <tristan.lins@bit3.de>
2013-10-24 12:31:46 +0200

    Add data definition container parser interfaces.

---
+++ system/modules/generalDriver/DcGeneral/DataDefinition/Parser/ContainerParserInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PHP version 5
+ * @package    generalDriver
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace DcGeneral\DataDefinition\Parser;
+
+interface ContainerParserInterface
+{
+	/**
+	 * Parse and return the container object.
+	 *
+	 * @return ContainerInterface
+	 */
+	public function parseContainer();
+}
