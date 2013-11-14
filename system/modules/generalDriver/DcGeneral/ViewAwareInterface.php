4c2a01658c6d57c12c61aa00026a6001ee8de362
Tristan Lins <tristan.lins@bit3.de>
2013-11-14 14:06:21 +0100

    Introduce ViewAwareInterface

---
+++ system/modules/generalDriver/DcGeneral/ViewAwareInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * PHP version 5
+ * @package    generalDriver
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace DcGeneral;
+
+use DcGeneral\View\ViewInterface;
+
+interface ViewAwareInterface
+{
+	/**
+	 * Return the view.
+	 *
+	 * @return ViewInterface
+	 */
+	public function getView();
+}
