6a1fb1fe7e85580150ec09dcd422d92228d470bc
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-11-18 08:28:12 +0100

    Removed various methods from DefaultController which are related to tree view and have been moved there or are obsolete.

--- system/modules/generalDriver/DcGeneral/Controller/ControllerInterface.php
+++ system/modules/generalDriver/DcGeneral/Controller/ControllerInterface.php
@@ -69,19 +69,6 @@ interface ControllerInterface
 	public function updateModelFromPropertyBag($model, $propertyValues);
 
 	/**
-	 * Recursively retrieve a collection of all complete node hierarchy.
-	 *
-	 * @param array $rootId        The ids of the root node.
-	 *
-	 * @param array $arrOpenParents The ids of the opened parent nodes. This may contain "all" to open all nodes.
-	 *
-	 * @param int $intLevel         The level the items are residing on.
-	 *
-	 * @return \DcGeneral\Data\CollectionInterface
-	 */
-	public function getTreeCollectionRecursive($rootId, $arrOpenParents, $intLevel = 0);
-
-	/**
 	 * Return all supported languages from the default data driver.
 	 *
 	 * @param mixed $mixID
