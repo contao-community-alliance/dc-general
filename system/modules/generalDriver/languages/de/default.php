821a602f3d7e3fd07f78491e35cec31125656047
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-11-26 18:46:20 +0100

    New translation key 'showSelected' for language switch button.

--- system/modules/generalDriver/languages/de/default.php
+++ system/modules/generalDriver/languages/de/default.php
@@ -1,5 +1,14 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
+/**
+ * PHP version 5
+ * @package    generalDriver
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
 
-$GLOBALS['TL_LANG']['MSC']['language'] = "Sprache";
-
-?>
+$GLOBALS['TL_LANG']['MSC']['language']     = 'Sprache';
+$GLOBALS['TL_LANG']['MSC']['showSelected'] = 'Sprache wechseln';
--- system/modules/generalDriver/languages/en/default.php
+++ system/modules/generalDriver/languages/en/default.php
@@ -1,5 +1,14 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
+/**
+ * PHP version 5
+ * @package    generalDriver
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
 
-$GLOBALS['TL_LANG']['MSC']['language'] = "Language";
-
-?>
+$GLOBALS['TL_LANG']['MSC']['language']     = 'Language';
+$GLOBALS['TL_LANG']['MSC']['showSelected'] = 'Switch language';
