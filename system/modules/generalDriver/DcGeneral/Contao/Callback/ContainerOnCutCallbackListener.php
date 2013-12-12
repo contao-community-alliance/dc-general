843b9e375e346e6f46e5cbab57a2e7a284d9970f
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-12-12 17:38:20 +0100

    phpDoc and code style in ContainerOnCutCallbackListener.

--- system/modules/generalDriver/DcGeneral/Contao/Callback/ContainerOnCutCallbackListener.php
+++ system/modules/generalDriver/DcGeneral/Contao/Callback/ContainerOnCutCallbackListener.php
@@ -14,9 +14,23 @@ namespace DcGeneral\Contao\Callback;
 
 use DcGeneral\DC_General;
 
+/**
+ * Class ContainerOnCutCallbackListener.
+ *
+ * Handle callbacks to be invoked when a cut operation is made.
+ *
+ * @package DcGeneral\Contao\Callback
+ */
 class ContainerOnCutCallbackListener extends AbstractStaticCallbackListener
 {
-	function __construct($callback, DC_General $dcGeneral)
+	/**
+	 * Create a new instance of the listener.
+	 *
+	 * @param array|callable $callback  The callback to call when invoked.
+	 *
+	 * @param DC_General     $dcGeneral The DC_General instance to use in the callback.
+	 */
+	public function __construct($callback, DC_General $dcGeneral)
 	{
 		parent::__construct($callback, $dcGeneral);
 	}
