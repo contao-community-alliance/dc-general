322e0a0b134ef7ab3a341d114e3700df4a108481
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-12-12 17:32:28 +0100

    phpDoc and code style in ContainerHeaderCallbackListener.

--- system/modules/generalDriver/DcGeneral/Contao/Callback/ContainerHeaderCallbackListener.php
+++ system/modules/generalDriver/DcGeneral/Contao/Callback/ContainerHeaderCallbackListener.php
@@ -14,23 +14,40 @@ namespace DcGeneral\Contao\Callback;
 
 use DcGeneral\Contao\View\Contao2BackendView\Event\GetParentHeaderEvent;
 use DcGeneral\DC_General;
-use DcGeneral\Event\PostDuplicateModelEvent;
 
+/**
+ * Class ContainerHeaderCallbackListener.
+ *
+ * Handler for the header row in parent list mode.
+ *
+ * @package DcGeneral\Contao\Callback
+ */
 class ContainerHeaderCallbackListener extends AbstractReturningCallbackListener
 {
 	/**
-	 * @var DC_General
+	 * The DC_General instance.
+	 *
+	 * @var \DcGeneral\DC_General
 	 */
 	protected $dcGeneral;
 
-	function __construct($callback, DC_General $dcGeneral)
+	/**
+	 * Create a new instance of the listener.
+	 *
+	 * @param array|callable $callback  The callback to call when invoked.
+	 *
+	 * @param DC_General     $dcGeneral The DC_General instance to use in the callback.
+	 */
+	public function __construct($callback, DC_General $dcGeneral)
 	{
 		parent::__construct($callback);
 		$this->dcGeneral = $dcGeneral;
 	}
 
 	/**
-	 * @param GetParentHeaderEvent $event
+	 * Retrieve the arguments for the callback.
+	 *
+	 * @param GetParentHeaderEvent $event The event being emitted.
 	 *
 	 * @return array
 	 */
@@ -40,13 +57,18 @@ class ContainerHeaderCallbackListener extends AbstractReturningCallbackListener
 	}
 
 	/**
-	 * @param GetParentHeaderEvent $event
+	 * Update the event with the information returned by the callback.
+	 *
+	 * @param GetParentHeaderEvent $event The event being emitted.
+	 *
+	 * @param array                $value The additional information.
 	 *
 	 * @return void
 	 */
 	public function update($event, $value)
 	{
-		if (is_null($value)) {
+		if (is_null($value))
+		{
 			return;
 		}
 
