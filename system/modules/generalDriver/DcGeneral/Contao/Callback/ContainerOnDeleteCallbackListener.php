21fee0e81bbb5bac969852933860df641d3bf777
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-12-12 17:56:51 +0100

    phpDoc and code style in ContainerOnDeleteCallbackListener.

--- system/modules/generalDriver/DcGeneral/Contao/Callback/ContainerOnDeleteCallbackListener.php
+++ system/modules/generalDriver/DcGeneral/Contao/Callback/ContainerOnDeleteCallbackListener.php
@@ -15,21 +15,39 @@ namespace DcGeneral\Contao\Callback;
 use DcGeneral\DC_General;
 use DcGeneral\Event\PostDeleteModelEvent;
 
+/**
+ * Class ContainerOnDeleteCallbackListener.
+ *
+ * Handler for delete callbacks.
+ *
+ * @package DcGeneral\Contao\Callback
+ */
 class ContainerOnDeleteCallbackListener extends AbstractCallbackListener
 {
 	/**
+	 * The DC_General instance.
+	 *
 	 * @var DC_General
 	 */
 	protected $dcGeneral;
 
-	function __construct($callback, DC_General $dcGeneral)
+	/**
+	 * Create a new instance of the listener.
+	 *
+	 * @param array|callable $callback  The callback to call when invoked.
+	 *
+	 * @param DC_General     $dcGeneral The DC_General instance to use in the callback.
+	 */
+	public function __construct($callback, DC_General $dcGeneral)
 	{
 		parent::__construct($callback);
 		$this->dcGeneral = $dcGeneral;
 	}
 
 	/**
-	 * @param PostDeleteModelEvent $event
+	 * Retrieve the arguments for the callback.
+	 *
+	 * @param PostDeleteModelEvent $event The event being emitted.
 	 *
 	 * @return array
 	 */
