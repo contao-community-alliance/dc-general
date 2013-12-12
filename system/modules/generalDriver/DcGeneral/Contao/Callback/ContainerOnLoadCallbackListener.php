e600e705ea1e6245d0ef114997154cfd7caefa92
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-12-12 18:11:48 +0100

    phpDoc and code style in ContainerOnLoadCallbackListener.

--- system/modules/generalDriver/DcGeneral/Contao/Callback/ContainerOnLoadCallbackListener.php
+++ system/modules/generalDriver/DcGeneral/Contao/Callback/ContainerOnLoadCallbackListener.php
@@ -14,8 +14,22 @@ namespace DcGeneral\Contao\Callback;
 
 use DcGeneral\DC_General;
 
+/**
+ * Class ContainerOnLoadCallbackListener.
+ *
+ * Handle onload_callbacks.
+ *
+ * @package DcGeneral\Contao\Callback
+ */
 class ContainerOnLoadCallbackListener extends AbstractStaticCallbackListener
 {
+	/**
+	 * Create a new instance of the listener.
+	 *
+	 * @param array|callable $callback  The callback to call when invoked.
+	 *
+	 * @param DC_General     $dcGeneral The DC_General instance to use in the callback.
+	 */
 	function __construct($callback, DC_General $dcGeneral)
 	{
 		parent::__construct($callback, $dcGeneral);
