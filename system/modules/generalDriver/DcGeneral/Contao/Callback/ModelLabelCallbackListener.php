f00a745009019606f9815c786e2bd621f76ac3d1
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-12-12 18:37:48 +0100

    phpDoc and code style in ModelLabelCallbackListener.

--- system/modules/generalDriver/DcGeneral/Contao/Callback/ModelLabelCallbackListener.php
+++ system/modules/generalDriver/DcGeneral/Contao/Callback/ModelLabelCallbackListener.php
@@ -15,18 +15,39 @@ namespace DcGeneral\Contao\Callback;
 use DcGeneral\Contao\View\Contao2BackendView\Event\ModelToLabelEvent;
 use DcGeneral\DC_General;
 
+/**
+ * Class ModelLabelCallbackListener.
+ *
+ * Handle the label_callbacks.
+ *
+ * @package DcGeneral\Contao\Callback
+ */
 class ModelLabelCallbackListener extends AbstractReturningCallbackListener
 {
+	/**
+	 * The DC_General instance.
+	 *
+	 * @var DC_General
+	 */
 	protected $dcGeneral;
 
-	function __construct($callback, DC_General $dcGeneral)
+	/**
+	 * Create a new instance of the listener.
+	 *
+	 * @param array|callable $callback  The callback to call when invoked.
+	 *
+	 * @param DC_General     $dcGeneral The DC_General instance to use in the callback.
+	 */
+	public function __construct($callback, DC_General $dcGeneral)
 	{
 		parent::__construct($callback);
 		$this->dcGeneral = $dcGeneral;
 	}
 
 	/**
-	 * @param ModelToLabelEvent $event
+	 * Retrieve the arguments for the callback.
+	 *
+	 * @param ModelToLabelEvent $event The event being emitted.
 	 *
 	 * @return array
 	 */
@@ -41,15 +62,18 @@ class ModelLabelCallbackListener extends AbstractReturningCallbackListener
 	}
 
 	/**
-	 * @param ModelToLabelEvent $event
+	 * Set the value in the event.
 	 *
-	 * @param                   $value
+	 * @param ModelToLabelEvent $event The event being emitted.
+	 *
+	 * @param string            $value The label text to use.
 	 *
 	 * @return void
 	 */
 	public function update($event, $value)
 	{
-		if (is_null($value)) {
+		if (is_null($value))
+		{
 			return;
 		}
 
