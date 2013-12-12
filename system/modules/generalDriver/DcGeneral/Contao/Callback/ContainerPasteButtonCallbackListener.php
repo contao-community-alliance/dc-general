600b9e53c695cfd0d5a9b177cc2a177a119b8468
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-12-12 18:18:32 +0100

    phpDoc and code style in ContainerPasteButtonCallbackListener.

--- system/modules/generalDriver/DcGeneral/Contao/Callback/ContainerPasteButtonCallbackListener.php
+++ system/modules/generalDriver/DcGeneral/Contao/Callback/ContainerPasteButtonCallbackListener.php
@@ -16,18 +16,39 @@ use DcGeneral\Contao\View\Contao2BackendView\Event\GetPasteButtonEvent;
 use DcGeneral\Contao\View\Contao2BackendView\Event\GetPasteRootButtonEvent;
 use DcGeneral\DC_General;
 
+/**
+ * Class ContainerPasteButtonCallbackListener.
+ *
+ * Invoker for paste_button_callbacks.
+ *
+ * @package DcGeneral\Contao\Callback
+ */
 class ContainerPasteButtonCallbackListener extends AbstractReturningCallbackListener
 {
+	/**
+	 * The DC_General instance.
+	 *
+	 * @var DC_General
+	 */
 	protected $dcGeneral;
 
-	function __construct($callback, DC_General $dcGeneral)
+	/**
+	 * Create a new instance of the listener.
+	 *
+	 * @param array|callable $callback  The callback to call when invoked.
+	 *
+	 * @param DC_General     $dcGeneral The DC_General instance to use in the callback.
+	 */
+	public function __construct($callback, DC_General $dcGeneral)
 	{
 		parent::__construct($callback);
 		$this->dcGeneral = $dcGeneral;
 	}
 
 	/**
-	 * @param GetPasteButtonEvent|GetPasteRootButtonEvent $event
+	 * Retrieve the arguments for the callback.
+	 *
+	 * @param GetPasteButtonEvent|GetPasteRootButtonEvent $event The event being emitted.
 	 *
 	 * @return array
 	 */
@@ -45,13 +66,18 @@ class ContainerPasteButtonCallbackListener extends AbstractReturningCallbackList
 	}
 
 	/**
-	 * @param GetPasteButtonEvent|GetPasteRootButtonEvent $event
+	 * Set the HTML code for the button.
+	 *
+	 * @param GetPasteButtonEvent|GetPasteRootButtonEvent $event The event being emitted.
+	 *
+	 * @param string                                      $value The value returned by the callback.
 	 *
 	 * @return void
 	 */
 	public function update($event, $value)
 	{
-		if (is_null($value)) {
+		if (is_null($value))
+		{
 			return;
 		}
 
