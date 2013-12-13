f63f972e417d98702d440d0188525fd046cdef94
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-12-13 19:32:33 +0100

    phpDoc and code style in in HardCodedPopulator.

--- system/modules/generalDriver/DcGeneral/Contao/Dca/Populator/HardCodedPopulator.php
+++ system/modules/generalDriver/DcGeneral/Contao/Dca/Populator/HardCodedPopulator.php
@@ -11,10 +11,10 @@ use DcGeneral\EnvironmentPopulator\AbstractEventDrivenEnvironmentPopulator;
 use DcGeneral\Contao\View\Contao2BackendView;
 
 /**
- * Class HardCodedPopulator
+ * Class HardCodedPopulator.
  *
- * This class only exists to have some intermediate hardcoded transition point until the builder ans populators have been
- * properly coded. This class will then be removed from the code base.
+ * This class only exists to have some intermediate hardcoded transition point until the builder ans populators have
+ * been properly coded. This class will then be removed from the code base.
  *
  * @package DcGeneral\Contao\Dca\Populator
  */
@@ -27,9 +27,10 @@ class HardCodedPopulator extends AbstractEventDrivenEnvironmentPopulator
 	 *
 	 * NOTE: callback classes are deprecated due to the events used in DcGeneral.
 	 *
-	 * @param EnvironmentInterface $environment
+	 * @param EnvironmentInterface $environment The environment to populate.
 	 *
-	 * @throws \DcGeneral\Exception\DcGeneralInvalidArgumentException
+	 * @return void
+	 *
 	 * @internal
 	 */
 	protected function populateCallback(EnvironmentInterface $environment)
@@ -50,7 +51,10 @@ class HardCodedPopulator extends AbstractEventDrivenEnvironmentPopulator
 	/**
 	 * Create a controller instance in the environment if none has been defined yet.
 	 *
-	 * @param EnvironmentInterface $environment
+	 * @param EnvironmentInterface $environment The environment to populate.
+	 *
+	 * @return void
+	 *
 	 * @internal
 	 */
 	public function populateController(EnvironmentInterface $environment)
