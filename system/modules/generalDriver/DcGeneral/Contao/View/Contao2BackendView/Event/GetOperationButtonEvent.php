97f66161e6c5b8e15836c655ecf4e500a882912a
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-12-16 15:51:39 +0100

    phpDoc and code style in in GetOperationButtonEvent.

--- system/modules/generalDriver/DcGeneral/Contao/View/Contao2BackendView/Event/GetOperationButtonEvent.php
+++ system/modules/generalDriver/DcGeneral/Contao/View/Contao2BackendView/Event/GetOperationButtonEvent.php
@@ -12,48 +12,75 @@
 
 namespace DcGeneral\Contao\View\Contao2BackendView\Event;
 
+/**
+ * Class GetOperationButtonEvent.
+ *
+ * This event gets emitted when an operation button is rendered.
+ *
+ * @package DcGeneral\Contao\View\Contao2BackendView\Event
+ */
 class GetOperationButtonEvent
 	extends BaseButtonEvent
 {
 	const NAME = 'dc-general.view.contao2backend.get-operation-button';
 
 	/**
+	 * The command for which the button is being rendered.
+	 *
 	 * @var \DcGeneral\DataDefinition\Definition\View\CommandInterface
 	 */
 	protected $command;
 
 	/**
+	 * The model to which the command shall be applied to.
+	 *
 	 * @var \DcGeneral\Data\ModelInterface
 	 */
 	protected $model;
 
 	/**
+	 * The ids of any child records of the model.
+	 *
 	 * @var array
 	 */
 	protected $childRecordIds;
 
 	/**
+	 * Determinator if there is a circular reference from an item in the clipboard to the current model.
+	 *
 	 * @var bool
 	 */
 	protected $circularReference;
 
 	/**
+	 * The id of the next model succeeding the current model.
+	 *
+	 * TODO: should this rather be the real model? Might be of more use.
+	 *
 	 * @var string
 	 */
 	protected $next;
 
 	/**
+	 * The id of the model preceeding the current model.
+	 *
+	 * TODO: should this rather be the real model? Might be of more use.
+	 *
 	 * @var string
 	 */
 	protected $previous;
 
 	/**
+	 * The href for the command.
+	 *
 	 * @var string
 	 */
 	protected $href;
 
 	/**
-	 * @param \DcGeneral\DataDefinition\Definition\View\CommandInterface
+	 * Set the attached command.
+	 *
+	 * @param \DcGeneral\DataDefinition\Definition\View\CommandInterface $objCommand The command.
 	 *
 	 * @return $this
 	 */
@@ -65,6 +92,8 @@ class GetOperationButtonEvent
 	}
 
 	/**
+	 * Retrieve the attached command.
+	 *
 	 * @return \DcGeneral\DataDefinition\Definition\View\CommandInterface
 	 */
 	public function getCommand()
@@ -73,7 +102,9 @@ class GetOperationButtonEvent
 	}
 
 	/**
-	 * @param array $childRecordIds
+	 * Set the ids of the child records of the current model.
+	 *
+	 * @param array $childRecordIds The list of ids.
 	 *
 	 * @return $this
 	 */
@@ -85,6 +116,8 @@ class GetOperationButtonEvent
 	}
 
 	/**
+	 * Retrieve the ids of the child records of the current model.
+	 *
 	 * @return array
 	 */
 	public function getChildRecordIds()
@@ -93,7 +126,12 @@ class GetOperationButtonEvent
 	}
 
 	/**
-	 * @param boolean $circularReference
+	 * Set determinator if there exists a circular reference.
+	 *
+	 * This flag determines if there exists a circular reference between the item currently in the clipboard and the
+	 * current model. A circular reference is of relevance when performing a cut and paste operation for example.
+	 *
+	 * @param boolean $circularReference The flag.
 	 *
 	 * @return $this
 	 */
@@ -105,6 +143,11 @@ class GetOperationButtonEvent
 	}
 
 	/**
+	 * Get determinator if there exists a circular reference.
+	 *
+	 * This flag determines if there exists a circular reference between the item currently in the clipboard and the
+	 * current model. A circular reference is of relevance when performing a cut and paste operation for example.
+	 *
 	 * @return boolean
 	 */
 	public function getCircularReference()
@@ -113,7 +156,9 @@ class GetOperationButtonEvent
 	}
 
 	/**
-	 * @param string $next
+	 * Set the id of the next model in the list, succeeding the current model.
+	 *
+	 * @param string $next The id of the successor.
 	 *
 	 * @return $this
 	 */
@@ -125,6 +170,8 @@ class GetOperationButtonEvent
 	}
 
 	/**
+	 * Get the id of the next model in the list, succeeding the current model.
+	 *
 	 * @return string
 	 */
 	public function getNext()
@@ -133,7 +180,9 @@ class GetOperationButtonEvent
 	}
 
 	/**
-	 * @param string $previous
+	 * Set the id of the previous model in the list, preceding the current model.
+	 *
+	 * @param string $previous The id of the predecessor.
 	 *
 	 * @return $this
 	 */
@@ -145,6 +194,8 @@ class GetOperationButtonEvent
 	}
 
 	/**
+	 * Get the id of the previous model in the list, preceding the current model.
+	 *
 	 * @return string
 	 */
 	public function getPrevious()
@@ -153,7 +204,9 @@ class GetOperationButtonEvent
 	}
 
 	/**
-	 * @param string $href
+	 * Set the href for the button.
+	 *
+	 * @param string $href The href.
 	 *
 	 * @return $this
 	 */
@@ -165,6 +218,8 @@ class GetOperationButtonEvent
 	}
 
 	/**
+	 * Retrieve the href for the button.
+	 *
 	 * @return string
 	 */
 	public function getHref()
@@ -173,7 +228,9 @@ class GetOperationButtonEvent
 	}
 
 	/**
-	 * @param \DcGeneral\Data\ModelInterface $model
+	 * Set the model currently in scope.
+	 *
+	 * @param \DcGeneral\Data\ModelInterface $model The model.
 	 *
 	 * @return $this
 	 */
@@ -185,6 +242,8 @@ class GetOperationButtonEvent
 	}
 
 	/**
+	 * Get the model currently in scope.
+	 *
 	 * @return \DcGeneral\Data\ModelInterface
 	 */
 	public function getModel()
