460d2f9480c096274b0c0f3b0ef8712ec0441b57
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-12-20 19:33:47 +0100

    phpDoc and code style in in FilterElementInformationInterface.

--- system/modules/generalDriver/DcGeneral/DataDefinition/Definition/View/Panel/ElementInformationInterface.php
+++ system/modules/generalDriver/DcGeneral/DataDefinition/Definition/View/Panel/ElementInformationInterface.php
@@ -2,6 +2,13 @@
 
 namespace DcGeneral\DataDefinition\Definition\View\Panel;
 
+/**
+ * Interface ElementInformationInterface.
+ *
+ * This interface describes a generic panel element information.
+ *
+ * @package DcGeneral\DataDefinition\Definition\View\Panel
+ */
 interface ElementInformationInterface
 {
 	/**
