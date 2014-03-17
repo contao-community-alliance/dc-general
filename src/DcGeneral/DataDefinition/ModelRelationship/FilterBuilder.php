d702048d1152feeb2d3b71b3aad2a03ff4c3dff5
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-03-17 14:11:46 +0100

    Add FilterBuilder and helper classes to simplify management of filter conditions.
    The new FilterBuilder classes allow the manipulation of parent->child
    and root filter conditions in data definitions.

---
+++ src/DcGeneral/DataDefinition/ModelRelationship/FilterBuilder.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * PHP version 5
+ * @package    DcGeneral
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  The Contao Community Alliance.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace DcGeneral\DataDefinition\ModelRelationship;
+
+use DcGeneral\DataDefinition\ModelRelationship\FilterBuilder\AndFilterBuilder;
+use DcGeneral\DataDefinition\ModelRelationship\FilterBuilder\BaseFilterBuilder;
+use DcGeneral\DataDefinition\ModelRelationship\FilterBuilder\OrFilterBuilder;
+use DcGeneral\DataDefinition\ModelRelationship\FilterBuilder\PropertyEqualsFilterBuilder;
+use DcGeneral\DataDefinition\ModelRelationship\FilterBuilder\PropertyGreaterThanFilterBuilder;
+use DcGeneral\DataDefinition\ModelRelationship\FilterBuilder\PropertyLessThanFilterBuilder;
+use DcGeneral\DataDefinition\ModelRelationship\FilterBuilder\PropertyValueInFilterBuilder;
+use DcGeneral\DataDefinition\ModelRelationship\FilterBuilder\PropertyValueLikeFilterBuilder;
+use DcGeneral\Exception\DcGeneralInvalidArgumentException;
+
+/**
+ * Handy helper class to generate and manipulate filter arrays.
+ *
+ * @package DcGeneral\DataDefinition\ModelRelationship
+ */
+class FilterBuilder
+{
+	/**
+	 * The current filter root (always an AND builder).
+	 *
+	 * @var AndFilterBuilder
+	 */
+	protected $filter;
+	/**
+	 * Flag determining if the current filter is a root filter or parent child filter.
+	 *
+	 * @var bool
+	 */
+	protected $isRootFilter;
+
+	/**
+	 * Create a new instance.
+	 *
+	 * @param array $filter Optional base filter array.
+	 *
+	 * @param bool  $isRoot Flag determining if the current filter is a root filter.
+	 *
+	 * @throws DcGeneralInvalidArgumentException When an invalid filter array has been passed.
+	 */
+	public function __construct($filter = array(), $isRoot = false)
+	{
+		if (!is_array($filter))
+		{
+			throw new DcGeneralInvalidArgumentException(
+				'FilterBuilder needs a valid filter array ' . gettype($filter) . 'given'
+			);
+		}
+
+		$this->filters = $this->getBuilderFromArray(array('operation' => 'AND', 'children' => $filter), $this);
+	}
+
+	/**
+	 * Instantiate the correct builder class from a given filter array.
+	 *
+	 * @param array         $filter  The filter.
+	 *
+	 * @param FilterBuilder $builder The builder instance.
+	 *
+	 * @return BaseFilterBuilder
+	 *
+	 * @throws DcGeneralInvalidArgumentException When an invalid operation is encountered.
+	 */
+	public static function getBuilderFromArray($filter, $builder)
+	{
+		switch ($filter['operation'])
+		{
+			case 'AND':
+				return AndFilterBuilder::fromArray($filter, $builder);
+			case 'OR':
+				return OrFilterBuilder::fromArray($filter, $builder);
+			case '=':
+				return PropertyEqualsFilterBuilder::fromArray($filter, $builder);
+			case '>':
+				return PropertyGreaterThanFilterBuilder::fromArray($filter, $builder);
+			case '<':
+				return PropertyLessThanFilterBuilder::fromArray($filter, $builder);
+			case 'IN':
+				return PropertyValueInFilterBuilder::fromArray($filter, $builder);
+			case 'LIKE':
+				return PropertyValueLikeFilterBuilder::fromArray($filter, $builder);
+			default:
+		}
+
+		throw new DcGeneralInvalidArgumentException(
+			'Invalid operation ' . $filter['operation'] . ' it must be one of: AND, OR, =, >, <, IN, LIKE'
+		);
+	}
+
+	/**
+	 * Create a new instance from an array.
+	 *
+	 * @param array $filter The initial filter array (optional).
+	 *
+	 * @return FilterBuilder
+	 */
+	public static function fromArray($filter = array())
+	{
+		return new static($filter, false);
+	}
+
+	/**
+	 * Create a new instance from an array for a root filter.
+	 *
+	 * @param array $filter The initial filter array (optional).
+	 *
+	 * @return FilterBuilder
+	 */
+	public static function fromArrayForRoot($filter = array())
+	{
+		return new static($filter, true);
+	}
+
+	/**
+	 * Return the root AND condition.
+	 *
+	 * @return AndFilterBuilder
+	 */
+	public function getFilter()
+	{
+		return $this->filters;
+	}
+
+	/**
+	 * Check that the builder is not for a root filter.
+	 *
+	 * @return FilterBuilder
+	 *
+	 * @throws DcGeneralInvalidArgumentException When the builder is for an root filter.
+	 */
+	public function checkNotRoot()
+	{
+		if (!$this->isRootFilter)
+		{
+			throw new DcGeneralInvalidArgumentException(
+				'ERROR: Filter builder is for an root filter.'
+			);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Return the current filters.
+	 *
+	 * @return array
+	 */
+	public function getAllAsArray()
+	{
+		$array = $this->filters->get();
+
+		return $array['children'];
+	}
+
+	/**
+	 * Check if an given argument is a valid operation.
+	 *
+	 * @param string $operation The operation to check.
+	 *
+	 * @return bool
+	 */
+	public static function isValidOperation($operation)
+	{
+		return in_array($operation, array('AND', 'OR', '=', '>', '<', 'IN', 'LIKE'));
+	}
+
+	/**
+	 * Check that an given argument is a valid operation.
+	 *
+	 * @param string $operation The operation to check.
+	 *
+	 * @return FilterBuilder
+	 *
+	 * @throws DcGeneralInvalidArgumentException When an invalid operation name has been passed.
+	 */
+	public function checkValidOperation($operation)
+	{
+		if (!$this->isValidOperation($operation))
+		{
+			throw new DcGeneralInvalidArgumentException(
+				'Invalid operation ' . $operation . ' it must be one of: AND, OR, =, >, <, IN, LIKE'
+			);
+		}
+
+		return $this;
+	}
+}
---
+++ src/DcGeneral/DataDefinition/ModelRelationship/FilterBuilder/BaseComparingFilterBuilder.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * PHP version 5
+ * @package    DcGeneral
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  The Contao Community Alliance.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace DcGeneral\DataDefinition\ModelRelationship\FilterBuilder;
+
+/**
+ * Handy helper class to generate and manipulate AND filter arrays.
+ *
+ * This class is intended to be only used via the FilterBuilder main class.
+ *
+ * @package DcGeneral\DataDefinition\ModelRelationship\FilterBuilder
+ */
+class BaseComparingFilterBuilder
+	extends BaseFilterBuilder
+{
+	/**
+	 * The operation string.
+	 *
+	 * @var string
+	 */
+	protected $operation;
+
+	/**
+	 * The property to be checked.
+	 *
+	 * @var string
+	 */
+	protected $property;
+
+	/**
+	 * The value to compare against.
+	 *
+	 * @var mixed
+	 */
+	protected $value;
+
+	/**
+	 * Flag determining if the passed value is a remote property name or not.
+	 *
+	 * @var bool
+	 */
+	protected $isRemote;
+
+	/**
+	 * Create a new instance.
+	 *
+	 * @param string $property The property name to be compared.
+	 *
+	 * @param mixed  $value    The value to be compared against.
+	 *
+	 * @param bool   $isRemote Flag determining if the passed value is a remote property name (only valid if filter is
+	 *                         for parent child relationship and not for root elements).
+	 */
+	public function __construct($property, $value, $isRemote = false)
+	{
+		$this->property = $property;
+		$this->value    = $value;
+		$this->isRemote = $isRemote;
+	}
+
+	/**
+	 * Initialize an instance with the values from the given array.
+	 *
+	 * @param array $array The initialization array.
+	 *
+	 * @return mixed
+	 */
+	public static function fromArray($array)
+	{
+		if (isset($array['remote']))
+		{
+			$value  = $array['remote'];
+			$remote = true;
+		}
+		else
+		{
+			$value  = $array['value'];
+			$remote = false;
+		}
+
+		return new static($array['property'], $value, $remote);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function get()
+	{
+		return array(
+			'operation' => $this->operation,
+			'property'  => $this->property,
+			($this->isRemote ? 'remote' : 'value') => $this->value
+		);
+	}
+
+	/**
+	 * Set the flag if this filter is for remote usage or not.
+	 *
+	 * @param boolean $isRemote The new flag.
+	 *
+	 * @return BaseComparingFilterBuilder
+	 */
+	public function setIsRemote($isRemote)
+	{
+		$this->isRemote = $isRemote;
+
+		return $this;
+	}
+
+	/**
+	 * Determine if this filter is for remote filtering or not.
+	 *
+	 * @return boolean
+	 */
+	public function isRemote()
+	{
+		return $this->isRemote;
+	}
+
+	/**
+	 * Set the property name.
+	 *
+	 * @param string $property The property name.
+	 *
+	 * @return BaseComparingFilterBuilder
+	 */
+	public function setProperty($property)
+	{
+		$this->property = $property;
+
+		return $this;
+	}
+
+	/**
+	 * Retrieve the property name.
+	 *
+	 * @return string
+	 */
+	public function getProperty()
+	{
+		return $this->property;
+	}
+
+	/**
+	 * Set the value to filter for.
+	 *
+	 * @param mixed $value The value.
+	 *
+	 * @return BaseComparingFilterBuilder
+	 */
+	public function setValue($value)
+	{
+		$this->value = $value;
+
+		return $this;
+	}
+
+	/**
+	 * Retrieve the value to filter for.
+	 *
+	 * @return mixed
+	 */
+	public function getValue()
+	{
+		return $this->value;
+	}
+}
