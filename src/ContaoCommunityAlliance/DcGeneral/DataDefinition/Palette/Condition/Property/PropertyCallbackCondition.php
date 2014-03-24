c8f5afee03caef0950be858b224ad8d32bb0036f
Tristan Lins <tristan.lins@bit3.de>
2014-03-24 11:08:32 +0100

    Add a new property callback condition.

---
+++ src/ContaoCommunityAlliance/DcGeneral/DataDefinition/Palette/Condition/Property/PropertyCallbackCondition.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * PHP version 5
+ * @package    generalDriver
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\DcGeneral\DataDefinition\Palette\Condition\Property;
+
+use ContaoCommunityAlliance\DcGeneral\Data\ModelInterface;
+use ContaoCommunityAlliance\DcGeneral\Data\PropertyValueBag;
+use ContaoCommunityAlliance\DcGeneral\DataDefinition\Palette\LegendInterface;
+use ContaoCommunityAlliance\DcGeneral\DataDefinition\Palette\PropertyInterface;
+
+/**
+ * Condition using a custom callback function for flexible custom conditions.
+ */
+class PropertyCallbackCondition implements PropertyConditionInterface
+{
+	/**
+	 * The callback.
+	 *
+	 * @var \Closure
+	 */
+	protected $callback;
+
+	/**
+	 * Create a new instance.
+	 *
+	 * @param \Closure $callback
+	 */
+	public function __construct($callback)
+	{
+		$this->callback = $callback;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function match(
+		ModelInterface $model = null,
+		PropertyValueBag $input = null,
+		PropertyInterface $property = null,
+		LegendInterface $legend = null
+	) {
+		return call_user_func($this->callback, $model, $input, $property, $legend);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __clone()
+	{
+	}
+}
