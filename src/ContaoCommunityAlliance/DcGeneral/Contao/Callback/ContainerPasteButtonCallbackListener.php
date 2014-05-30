8f4f45fcc12b8b62cee9b618d2b0de2d93ad2048
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-05-30 20:42:47 +0200

    Fix ContainerPasteButtonCallbackListener.

--- src/ContaoCommunityAlliance/DcGeneral/Contao/Callback/ContainerPasteButtonCallbackListener.php
+++ src/ContaoCommunityAlliance/DcGeneral/Contao/Callback/ContainerPasteButtonCallbackListener.php
@@ -39,8 +39,8 @@ class ContainerPasteButtonCallbackListener extends AbstractReturningCallbackList
 			$event->getEnvironment()->getDataDefinition()->getName(),
 			$event->getCircularReference(),
 			$event->getEnvironment()->getClipboard()->getContainedIds(),
-			$event->getPrevious()->getId(),
-			$event->getNext()->getId()
+			$event->getPrevious() ? $event->getPrevious()->getId() : null,
+			$event->getNext() ? $event->getNext()->getId() : null
 		);
 	}
 
