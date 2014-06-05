494d1219b2df45b4de9374efe98629d2d4719337
Stefan Heimes <stefan_heimes@hotmail.com>
2014-06-05 22:56:53 +0200

    Add a new collection and interface for the filter options. Now it is possible to add filter options as key => value pair to the collection.

---
+++ src/ContaoCommunityAlliance/DcGeneral/Data/FilterOptionCollectionInterface.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * PHP version 5
+ * @package    generalDriver
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\DcGeneral\Data;
+
+/**
+ * This represents an iterable collection of Model elements.
+ */
+interface FilterOptionCollectionInterface
+	extends \IteratorAggregate, \Countable
+{
+	/**
+	 * Append a key => value pair.
+	 *
+	 * @param string $filterKey   The key of the filter option. Needed for the system
+	 *
+	 * @param string $filterValue The readable value for humans.
+	 *
+	 * @return FilterOptionCollectionInterface
+	 */
+	public function add($filterKey, $filterValue);
+}
