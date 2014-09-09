1e3f991f290bc0bfc24766073ed1ac3e61533cb7
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-09-09 18:58:12 +0200

    Rename $propertyInformation to $groupAndSortingInformation.

--- src/ContaoCommunityAlliance/DcGeneral/Contao/Dca/Builder/Legacy/LegacyDcaDataDefinitionBuilder.php
+++ src/ContaoCommunityAlliance/DcGeneral/Contao/Dca/Builder/Legacy/LegacyDcaDataDefinitionBuilder.php
@@ -938,34 +938,39 @@ class LegacyDcaDataDefinitionBuilder extends DcaReadingDataDefinitionBuilder
 
 		foreach ($sortingDca['fields'] as $field)
 		{
-			$propertyInformation = $definition->add();
+			$groupAndSortingInformation = $definition->add();
 
 			if (isset($sortingDca['flag']))
 			{
-				$this->evalFlag($propertyInformation, $sortingDca['flag']);
+				$this->evalFlag($groupAndSortingInformation, $sortingDca['flag']);
 			}
 
 			if (preg_match('~^(\w+)(?: (.+))?$~', $field, $matches))
 			{
-				$propertyInformation
+				$groupAndSortingInformation
 					->setProperty($matches[1])
-					->setSortingMode(isset($matches[2]) ? $matches[2] : GroupAndSortingInformationInterface::SORT_ASC);
+					->setSortingMode(
+						isset($matches[2])
+						? $matches[2]
+						: GroupAndSortingInformationInterface::SORT_ASC
+					);
 			}
 			else
 			{
 				throw new DcGeneralRuntimeException('Custom SQL in sorting fields are currently unsupported');
 			}
 
-			if (isset($fieldsDca[$propertyInformation->getProperty()])) {
-				if (isset($fieldsDca[$propertyInformation->getProperty()]['flag'])) {
-					$this->evalFlagGrouping($propertyInformation, $fieldsDca[$propertyInformation->getProperty()]['flag']);
-					$this->evalFlagGroupingLength($propertyInformation, $fieldsDca[$propertyInformation->getProperty()]['flag']);
+			if (isset($fieldsDca[$groupAndSortingInformation->getProperty()])) {
+				if (isset($fieldsDca[$groupAndSortingInformation->getProperty()]['flag'])) {
+					$flag = $fieldsDca[$groupAndSortingInformation->getProperty()]['flag'];
+					$this->evalFlagGrouping($groupAndSortingInformation, $flag);
+					$this->evalFlagGroupingLength($groupAndSortingInformation, $flag);
 				}
 			}
 
 			if (isset($sortingDca['disableGrouping']) && $sortingDca['disableGrouping'])
 			{
-				$propertyInformation->setGroupingMode(ListingConfigInterface::GROUP_NONE);
+				$groupAndSortingInformation->setGroupingMode(GroupAndSortingInformationInterface::GROUP_NONE);
 			}
 		}
 	}
