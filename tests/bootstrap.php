1b42494050e24deb35c45d08eee512f2c8907e21
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-01-22 04:33:57 +0100

    Fix phpUnit base configuration.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,12 +15,16 @@ error_reporting(E_ALL);
 
 function includeIfExists($file)
 {
-	return file_exists($file) ? include $file : false;
+    return file_exists($file) ? include $file : false;
 }
 
-if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
-	echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
-		'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
-		'php composer.phar install'.PHP_EOL;
-	exit(1);
+if (
+    // Locally installed dependencies
+    (!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php'))
+    // We are within an composer install.
+    && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+    echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL;
+    exit(1);
 }
