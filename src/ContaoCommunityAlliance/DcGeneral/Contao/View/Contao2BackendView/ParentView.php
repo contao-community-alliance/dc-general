67fa60a7c9194f1da1b7de45c6edb9e2b9b98202
David Molineus <david.molineus@netzmacht.de>
2015-06-24 11:43:28 +0200

    Translate tstamp field.

--- src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/ParentView.php
+++ src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/ParentView.php
@@ -288,7 +288,12 @@ class ParentView extends BaseView
 
             // Add the sorting field.
             if ($value != '') {
-                $lang = $this->translate(sprintf('%s.0', $v), $parentName);
+                if ($v === 'tstamp') {
+                    $lang = $this->translate('tstamp', 'MSC');
+                } else {
+                    $lang = $this->translate(sprintf('%s.0', $v), $parentName);
+                }
+
                 $key  = $lang ? $lang : $v;
 
                 $add[$key] = $value;
