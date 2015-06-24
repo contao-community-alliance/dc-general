d7b2b860c4e0eff6db71e10ec5a3e4d1079d2bde
David Molineus <david.molineus@netzmacht.de>
2015-06-24 11:44:29 +0200

    Add author information.

--- src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/ParentView.php
+++ src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/ParentView.php
@@ -6,6 +6,7 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @author     David Molineus <david.molineus@netzmacht.de>
  * @copyright  The MetaModels team.
  * @license    LGPL.
  * @filesource
