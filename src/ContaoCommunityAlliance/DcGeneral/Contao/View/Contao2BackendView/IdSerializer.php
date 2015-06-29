547b0509bbc7d05daff3ea51d20813742de85280
David Molineus <david.molineus@netzmacht.de>
2015-06-29 07:42:15 +0200

    IdSerializer extends ModelId now.

--- src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/IdSerializer.php
+++ src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/IdSerializer.php
@@ -5,6 +5,7 @@
  * @package    generalDriver
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @author     David Molineus <david.molineus@netzmacht.de>
  * @copyright  The MetaModels team.
  * @license    LGPL.
  * @filesource
@@ -12,31 +13,16 @@
 
 namespace ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView;
 
-use ContaoCommunityAlliance\DcGeneral\Data\ModelInterface;
-use ContaoCommunityAlliance\DcGeneral\Exception\DcGeneralRuntimeException;
-
 /**
  * The class IdSerializer provides handy methods to serialize and un-serialize model ids including the data provider
  * name into a string.
  *
+ * @deprecated This class gonna be replaced by the ModelIdInterface. Use this instead!
+ *
  * @package DcGeneral\Contao\View\Contao2BackendView
  */
-class IdSerializer
+class IdSerializer extends ModelId
 {
-    /**
-     * The data provider name.
-     *
-     * @var string
-     */
-    protected $dataProviderName;
-
-    /**
-     * The id of the model.
-     *
-     * @var mixed
-     */
-    protected $modelId;
-
     /**
      * Set the data provider name.
      *
@@ -49,16 +35,6 @@ class IdSerializer
         $this->dataProviderName = $dataProviderName;
 
         return $this;
-    }
-
-    /**
-     * Retrieve the data provider name.
-     *
-     * @return string
-     */
-    public function getDataProviderName()
-    {
-        return $this->dataProviderName;
     }
 
     /**
@@ -76,96 +52,6 @@ class IdSerializer
     }
 
     /**
-     * Retrieve the id.
-     *
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->modelId;
-    }
-
-    /**
-     * Create an instance from the passed values.
-     *
-     * @param string $dataProviderName The data provider name.
-     *
-     * @param mixed  $modelId          The id.
-     *
-     * @return IdSerializer
-     */
-    public static function fromValues($dataProviderName, $modelId)
-    {
-        $instance = new IdSerializer();
-
-        $instance
-            ->setId($modelId)
-            ->setDataProviderName($dataProviderName);
-
-        return $instance;
-    }
-
-    /**
-     * Create an instance from a model.
-     *
-     * @param ModelInterface $model The model.
-     *
-     * @return IdSerializer
-     */
-    public static function fromModel(ModelInterface $model)
-    {
-        return self::fromValues($model->getProviderName(), $model->getId());
-    }
-
-    /**
-     * Create an instance from an serialized id.
-     *
-     * @param string $serialized The id.
-     *
-     * @return IdSerializer
-     *
-     * @throws DcGeneralRuntimeException When invalid data is encountered.
-     */
-    public static function fromSerialized($serialized)
-    {
-        $instance = new IdSerializer();
-
-        $serialized = rawurldecode($serialized);
-        $serialized = html_entity_decode($serialized, ENT_QUOTES, 'UTF-8');
-
-        $chunks = explode('::', $serialized);
-
-        if (count($chunks) !== 2) {
-            throw new DcGeneralRuntimeException('Unparsable encoded id value: ' . var_export($serialized, true));
-        }
-
-        $instance->setDataProviderName($chunks[0]);
-
-        if (is_numeric($chunks[1])) {
-            return $instance->setId($chunks[1]);
-        }
-
-        $decodedSource = base64_decode($chunks[1]);
-        $decodedJson   = json_decode($decodedSource, true);
-
-        return $instance->setId($decodedJson ?: $decodedSource);
-    }
-
-    /**
-     * Serialize the id.
-     *
-     * @return string
-     */
-    public function getSerialized()
-    {
-        if (is_numeric($this->modelId)) {
-            return sprintf('%s::%s', $this->dataProviderName, $this->modelId);
-        }
-
-        return sprintf('%s::%s', $this->dataProviderName, base64_encode(json_encode($this->modelId)));
-    }
-
-    /**
      * Determine if both, data provider name and id are set and non empty.
      *
      * @return bool
@@ -173,27 +59,5 @@ class IdSerializer
     public function isValid()
     {
         return !(empty($this->modelId) || empty($this->dataProviderName));
-    }
-
-    /**
-     * Determine if this id, is equals to the other id.
-     *
-     * @param IdSerializer $idSerializer The other id.
-     *
-     * @return bool
-     */
-    public function equals(IdSerializer $idSerializer)
-    {
-        // It is exactly the same id
-        if ($this === $idSerializer) {
-            return true;
-        }
-
-        return !(
-            // The data provider are not equal
-            $this->getDataProviderName() !== $idSerializer->getDataProviderName()
-            // The model ids are not equal
-            || $this->getId() !== $idSerializer->getId()
-        );
     }
 }
