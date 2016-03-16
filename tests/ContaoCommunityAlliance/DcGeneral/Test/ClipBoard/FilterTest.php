644a303ad69f9003b33df74636bcaecf3041c808
Stefan Heimes <stefan_heimes@hotmail.com>
2016-03-17 00:19:28 +0100

    Add missing author

--- tests/ContaoCommunityAlliance/DcGeneral/Test/ClipBoard/FilterTest.php
+++ tests/ContaoCommunityAlliance/DcGeneral/Test/ClipBoard/FilterTest.php
@@ -12,6 +12,7 @@
  *
  * @package    contao-community-alliance/dc-general
  * @author     David Molineus <david.molineus@netzmacht.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @copyright  2013-2015 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0
  * @filesource
