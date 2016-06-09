f37afc1ddd666af3995eed12f0636cf4bf7c5e8a
Ingolf Steinhardt <info@e-spin.de>
2016-06-09 09:37:29 +0200

    Filter empty values

--- src/ContaoCommunityAlliance/DcGeneral/Panel/DefaultFilterElement.php
+++ src/ContaoCommunityAlliance/DcGeneral/Panel/DefaultFilterElement.php
@@ -162,7 +162,7 @@ class DefaultFilterElement extends AbstractElement implements FilterElementInter
     {
         $this->updateValue();
 
-        if ($this->getPropertyName() && $this->getValue() && ($objElement !== $this)) {
+        if ($this->getPropertyName() && $this->getValue() !== NULL && ($objElement !== $this)) {
             $arrCurrent = $objConfig->getFilter();
             if (!is_array($arrCurrent)) {
                 $arrCurrent = array();
