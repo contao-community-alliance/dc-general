13fb49e8388a6ea5b85110496e77dd5b975c435c
binron <rtb@gmx.ch>
2016-09-07 22:40:06 +0200

    Added author

--- src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/ButtonRenderer.php
+++ src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/ButtonRenderer.php
@@ -12,6 +12,7 @@
  *
  * @package    contao-community-alliance/dc-general
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     binron <rtb@gmx.ch>
  * @copyright  2013-2015 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0
  * @filesource
