bacaa414397c9272c48ac4dbaf69838866941369
Ingolf Steinhardt <info@e-spin.de>
2016-11-25 18:21:12 +0100

    correct property datepicker statement

--- src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/ContaoWidgetManager.php
+++ src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/ContaoWidgetManager.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2015 Contao Community Alliance.
+ * (c) 2013-2016 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,7 @@
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2013-2015 Contao Community Alliance.
+ * @copyright  2013-2016 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -382,7 +382,7 @@ class ContaoWidgetManager
         }
 
         $strDatePicker = '';
-        if (isset($propExtra['datepicker'])) {
+        if (!empty($propExtra['datepicker']))) {
             $strDatePicker = $this->buildDatePicker($widget);
         }
 
