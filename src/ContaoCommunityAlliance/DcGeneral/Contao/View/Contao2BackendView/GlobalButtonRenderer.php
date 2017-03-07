311d968ce1131c346caf8a6aac37e28fd0502838
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-03-07 01:00:09 +0100

    Fix author validation

--- src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/GlobalButtonRenderer.php
+++ src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/GlobalButtonRenderer.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2015 Contao Community Alliance.
+ * (c) 2013-2017 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,8 +12,7 @@
  *
  * @package    contao-community-alliance/dc-general
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @author     binron <rtb@gmx.ch>
- * @copyright  2013-2015 Contao Community Alliance.
+ * @copyright  2013-2017 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
