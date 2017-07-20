99bf1942ef9d8e971c2636eb519022457c649e3e
Sven Baumann <baumann.sv@gmail.com>
2017-07-21 01:22:23 +0200

    Support contao managed edition

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dc-general.
+ *
+ * (c) 2013-2017 Contao Community Alliance.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dc-general
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2013-2017 Contao Community Alliance.
+ * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\DcGeneral\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerBundle\ContaoManagerBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ContaoCommunityAlliance\Contao\Bindings\CcaEventsContaoBindingsBundle;
+use ContaoCommunityAlliance\DcGeneral\DcGeneralBundle;
+use DependencyInjection\Container\CcaDependencyInjectionBundle;
+
+/**
+ * Plugin for the Contao Manager.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        // Todo if the url builder is bundle add it by load after.
+        return [
+            BundleConfig::create(DcGeneralBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class,
+                        ContaoManagerBundle::class,
+                        CcaDependencyInjectionBundle::class,
+                        CcaEventsContaoBindingsBundle::class
+                    ]
+                ),
+        ];
+    }
+}
