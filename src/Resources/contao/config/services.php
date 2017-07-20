6a58c83d7c70cfa960867323943355008918c10d
Sven Baumann <baumann.sv@gmail.com>
2017-07-21 01:39:31 +0200

    Register services as conform symfony

--- src/Resources/contao/config/services.php
+++ src/Resources/contao/config/services.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2015 Contao Community Alliance.
+ * (c) 2013-2017 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,38 +13,13 @@
  * @package    contao-community-alliance/dc-general
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Tristan Lins <tristan.lins@bit3.de>
- * @copyright  2013-2015 Contao Community Alliance.
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2013-2017 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
-/** @var Pimple $container */
-
-/**
- * This function creates the default data definition container.
- * To override the type, set your own implementation override the value of
- * $container['dc-general.data-definition-container.factory'].
- *
- * @return \ContaoCommunityAlliance\DcGeneral\DataDefinitionContainerInterface $container
- */
-$container['dc-general.data-definition-container.factory.default'] = $container->protect(
-    function () {
-        return new \ContaoCommunityAlliance\DcGeneral\DataDefinitionContainer();
-    }
-);
-
-if (!isset($container['dc-general.data-definition-container.factory'])) {
-    $container['dc-general.data-definition-container.factory'] =
-        $container->raw('dc-general.data-definition-container.factory.default');
-}
-
-$container['dc-general.data-definition-container'] = $container->share(
-    function ($container) {
-        $factory = $container['dc-general.data-definition-container.factory'];
-
-        /** @var \ContaoCommunityAlliance\DcGeneral\DataDefinitionContainerInterface $container */
-        $container = $factory();
-
-        return $container;
-    }
-);
+/** @var \DependencyInjection\Container\PimpleGate $container */
+$container->provideSymfonyService('dc-general.data-definition-container.factory.default');
+$container->provideSymfonyService('dc-general.data-definition-container.factory');
+$container->provideSymfonyService('dc-general.data-definition-container');
