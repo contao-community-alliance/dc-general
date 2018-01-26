fa4864c865cefeb226534037875765c10714b546
Sven Baumann <baumann.sv@gmail.com>
2018-01-26 20:21:24 +0100

    Filters pass defined property names in fetch, only real property is supported

--- src/ContaoCommunityAlliance/DcGeneral/Controller/ModelCollector.php
+++ src/ContaoCommunityAlliance/DcGeneral/Controller/ModelCollector.php
@@ -164,7 +164,23 @@ class ModelCollector
             throw new \InvalidArgumentException('Invalid provider name ' . $modelId->getDataProviderName());
         }
 
-        $config->setId($modelId->getId())->setFields($propertyDefinition->getPropertyNames());
+        $properties = [];
+        // Filter real properties from the property definition.
+        foreach ($propertyDefinition->getPropertyNames() as $propertyName) {
+            if ($dataProvider->fieldExists($propertyName)) {
+                $properties[] = $propertyName;
+
+                continue;
+            }
+
+            @trigger_error
+            (
+                'Only real property is allowed in the property definition.'.
+                'This will no longer be supported in the future.',
+                E_DEPRECATED
+            );
+        }
+        $config->setId($modelId->getId())->setFields($properties);
 
         return $dataProvider->fetch($config);
     }
