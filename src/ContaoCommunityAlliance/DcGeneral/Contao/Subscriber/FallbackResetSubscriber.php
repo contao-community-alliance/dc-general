66159cb6638de2734f69183c8adddc560e536949
Ingolf Steinhardt <info@e-spin.de>
2018-02-01 08:26:02 +0100

    Hotfix reset fallback PR #321

--- src/ContaoCommunityAlliance/DcGeneral/Contao/Subscriber/FallbackResetSubscriber.php
+++ src/ContaoCommunityAlliance/DcGeneral/Contao/Subscriber/FallbackResetSubscriber.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2015 Contao Community Alliance.
+ * (c) 2013-2018 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,8 +12,9 @@
  *
  * @package    contao-community-alliance/dc-general
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2013-2016 Contao Community Alliance.
- * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2013-2018 Contao Community Alliance.
+ * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
@@ -111,6 +112,11 @@ class FallbackResetSubscriber implements EventSubscriberInterface
                     // @codingStandardsIgnoreEnd
 
                     $dataProvider->resetFallback($propertyName);
+                }
+
+                // If a fallback already exists, skip the empty one.
+                if (!$model->getProperty($propertyName)) {
+                    continue;
                 }
 
                 $models = $dataProvider->fetchAll($config);
