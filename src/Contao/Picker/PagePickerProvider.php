df9c25b7444ed3fdbfcbe69b30d5b62dc96274ff
Sven Baumann <baumann.sv@gmail.com>
2018-03-28 22:48:55 +0200

    Not extend PagePickerProvider for php 7 compatibility and contao 4.5

--- src/Contao/Picker/PagePickerProvider.php
+++ src/Contao/Picker/PagePickerProvider.php
@@ -19,12 +19,14 @@
 
 namespace ContaoCommunityAlliance\DcGeneral\Contao\Picker;
 
-use Contao\CoreBundle\Picker\PagePickerProvider as ContaoPagePickerProvider;
+use Contao\CoreBundle\Picker\AbstractPickerProvider;
+use Contao\CoreBundle\Picker\DcaPickerProviderInterface;
+use Contao\CoreBundle\Picker\PickerConfig;
 
 /**
  * Provides the page picker.
  */
-class PagePickerProvider extends ContaoPagePickerProvider
+class PagePickerProvider extends AbstractPickerProvider implements DcaPickerProviderInterface
 {
     /**
      * {@inheritdoc}
@@ -40,5 +42,84 @@ class PagePickerProvider extends ContaoPagePickerProvider
     public function supportsContext($context)
     {
         return \in_array($context, ['cca_ page', 'cca_link'], true) && $this->getUser()->hasAccess('page', 'modules');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsValue(PickerConfig $config)
+    {
+        if ('page' === $config->getContext()) {
+            return \is_numeric($config->getValue());
+        }
+
+        return false !== \strpos($config->getValue(), '{{link_url::');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDcaTable()
+    {
+        return 'tl_page';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDcaAttributes(PickerConfig $config)
+    {
+        $value = $config->getValue();
+        $attributes = ['fieldType' => 'radio'];
+
+        if ('page' === $config->getContext()) {
+            if ($fieldType = $config->getExtra('fieldType')) {
+                $attributes['fieldType'] = $fieldType;
+            }
+
+            if ($source = $config->getExtra('source')) {
+                $attributes['preserveRecord'] = $source;
+            }
+
+            if (\is_array($rootNodes = $config->getExtra('rootNodes'))) {
+                $attributes['rootNodes'] = $rootNodes;
+            }
+
+            if ($value) {
+                $intval = function ($val) {
+                    return (int) $val;
+                };
+
+                $attributes['value'] = \array_map($intval, \explode(',', $value));
+            }
+
+            return $attributes;
+        }
+
+        if ($value && false !== \strpos ($value, '{{link_url::')) {
+            $attributes['value'] = \str_replace (['{{link_url::', '}}'], '', $value);
+        }
+
+        return $attributes;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertDcaValue(PickerConfig $config, $value)
+    {
+        if ('page' === $config->getContext()) {
+            return (int) $value;
+        }
+
+        return '{{link_url::'.$value.'}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getRouteParameters(PickerConfig $config = null)
+    {
+        return ['do' => 'page'];
     }
 }
