e08c9385dee352864ff4061265571776d5cf8fe7
Sven Baumann <baumann.sv@gmail.com>
2018-12-11 11:27:27 +0100

    Extract the get select mode button in a event

---
+++ src/ContaoCommunityAlliance/DcGeneral/Contao/View/Contao2BackendView/EventListener/SelectModeButtonsListener.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dc-general.
+ *
+ * (c) 2013-2018 Contao Community Alliance.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dc-general
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2013-2018 Contao Community Alliance.
+ * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\EventListener;
+
+use ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\Event\GetSelectModeButtonsEvent;
+use ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\ViewHelpers;
+
+/**
+ * This class handle for add the default buttons for the select mode.
+ */
+class SelectModeButtonsListener
+{
+    /**
+     * Handle event for add the default buttons for the select mode.
+     *
+     * @param GetSelectModeButtonsEvent $event The event.
+     *
+     * @return void
+     */
+    public function handleEvent(GetSelectModeButtonsEvent $event)
+    {
+        $translator      = $event->getEnvironment()->getTranslator();
+        $definition      = $event->getEnvironment()->getDataDefinition();
+        $basicDefinition = $definition->getBasicDefinition();
+        $buttons         = [];
+
+        $confirmMessage = \htmlentities(
+            \sprintf(
+                '<h2 class="tl_error">%s</h2>' .
+                '<p></p>' .
+                '<div class="tl_submit_container">' .
+                '<input type="submit" name="close" class="%s" value="%s" onclick="%s">' .
+                '</div>',
+                \specialchars($translator->translate('MSC.nothingSelect')),
+                'tl_submit',
+                \specialchars($translator->translate('MSC.close')),
+                'this.blur(); BackendGeneral.hideMessage(); return false;'
+            )
+        );
+        $onClick        = 'BackendGeneral.confirmSelectOverrideEditAll(this, \'models[]\', \''
+                          . $confirmMessage . '\'); return false;';
+
+        $input = '<input type="submit" name="%s" id="%s" class="tl_submit" accesskey="%s" value="%s" onclick="%s">';
+
+        if ($basicDefinition->isDeletable()) {
+            $onClickDelete = \sprintf(
+                'BackendGeneral.confirmSelectDeleteAll(this, \'%s\', \'%s\', \'%s\', \'%s\', \'%s\'); return false;',
+                'models[]',
+                $confirmMessage,
+                \specialchars($translator->translate('MSC.delAllConfirm')),
+                \specialchars($translator->translate('MSC.confirmOk')),
+                \specialchars($translator->translate('MSC.confirmAbort'))
+            );
+
+            $buttons['delete'] = \sprintf(
+                $input,
+                'delete',
+                'delete',
+                'd',
+                \specialchars($translator->translate('MSC.deleteSelected')),
+                $onClickDelete
+            );
+        }
+
+        $sortingProperty = ViewHelpers::getManualSortingProperty($event->getEnvironment());
+        if ($sortingProperty && $basicDefinition->isEditable()) {
+            $buttons['cut'] = \sprintf(
+                $input,
+                'cut',
+                'cut',
+                's',
+                \specialchars($translator->translate('MSC.moveSelected')),
+                $onClick
+            );
+        }
+
+        if ($basicDefinition->isCreatable()) {
+            $buttons['copy'] = \sprintf(
+                $input,
+                'copy',
+                'copy',
+                'c',
+                \specialchars($translator->translate('MSC.copySelected')),
+                $onClick
+            );
+        }
+
+        if ($basicDefinition->isEditable()) {
+            $buttons['override'] = \sprintf(
+                $input,
+                'override',
+                'override',
+                'v',
+                \specialchars($translator->translate('MSC.overrideSelected')),
+                $onClick
+            );
+
+            $buttons['edit'] = \sprintf(
+                $input,
+                'edit',
+                'edit',
+                's',
+                \specialchars($translator->translate('MSC.editSelected')),
+                $onClick
+            );
+        }
+
+        $event->setButtons($buttons);
+    }
+}
