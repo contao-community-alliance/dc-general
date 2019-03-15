9b8cf5503603f6536914c1641fb33b82cdfe4bdf
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2019-03-15 13:58:30 +0100

    Update copyright.

--- src/Data/DefaultDataProvider.php
+++ src/Data/DefaultDataProvider.php
@@ -22,6 +22,7 @@
  * @author     Christopher Boelter <christopher@boelter.eu>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @copyright  2013-2019 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
