85748c97317783d47a526bb309344d2bf098449a
Sven Baumann <baumann.sv@gmail.com>
2019-03-20 01:09:57 +0100

    Deprecated the file select

--- src/Contao/View/Contao2BackendView/FileSelect.php
+++ src/Contao/View/Contao2BackendView/FileSelect.php
@@ -47,6 +47,8 @@ use ContaoCommunityAlliance\Translator\TranslatorChain;
  * Class FileSelect.
  *
  * Back end tree picker for usage in generalfile.php.
+ *
+ * @deprecated This is deprecated since 2.1 and where removed in 3.0. Use the file tree widget instead.
  */
 class FileSelect
 {
