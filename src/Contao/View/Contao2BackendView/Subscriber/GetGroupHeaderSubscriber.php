fd2d3c18ec74d2334cf9c5ec6a976c398cc1522e
Cliff Parnitzky <github@cliff-parnitzky.de>
2020-05-08 12:31:58 +0200

    Fix travis

--- src/Contao/View/Contao2BackendView/Subscriber/GetGroupHeaderSubscriber.php
+++ src/Contao/View/Contao2BackendView/Subscriber/GetGroupHeaderSubscriber.php
@@ -15,7 +15,7 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @author     Cliff Parnitzky <contao@cliff-parnitzky.de>
+ * @author     Cliff Parnitzky <github@cliff-parnitzky.de>
  * @copyright  2013-2020 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
