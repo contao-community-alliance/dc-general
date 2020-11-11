a6d99f0466db78de50554348b6870071e4a68532
Sven Baumann <baumann.sv@gmail.com>
2020-11-11 12:11:19 +0100

    Disable the versioning in the legacy data definition if is enable. The versioning comes as feature in a later version. #553

--- src/Contao/Dca/Builder/Legacy/LegacyDcaDataDefinitionBuilder.php
+++ src/Contao/Dca/Builder/Legacy/LegacyDcaDataDefinitionBuilder.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2019 Contao Community Alliance.
+ * (c) 2013-2020 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2013-2019 Contao Community Alliance.
+ * @copyright  2013-2020 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -533,8 +533,15 @@ class LegacyDcaDataDefinitionBuilder extends DcaReadingDataDefinitionBuilder
                         )
                     );
             }
-            $providerInformation
-                ->setVersioningEnabled((bool) $this->getFromDca('config/enableVersioning'));
+            $providerInformation->setVersioningEnabled(false);
+            if (true === (bool) $this->getFromDca('config/enableVersioning')) {
+                // @codingStandardsIgnoreStart
+                @\trigger_error(
+                    'Versioning is not supported yet and will get implemented in a future release.',
+                    E_USER_WARNING
+                );
+                // @codingStandardsIgnoreEnd
+            }
 
             if (!$container->getBasicDefinition()->getDataProvider()) {
                 $container->getBasicDefinition()->setDataProvider($providerName);
