c20528d03bf8e10c53ff18598514b296dd36830f
Sven Baumann <baumann.sv@gmail.com>
2020-11-11 12:57:01 +0100

    Supress phpmd  warnings of long classname

--- src/Contao/View/Contao2BackendView/ContaoWidgetManager.php
+++ src/Contao/View/Contao2BackendView/ContaoWidgetManager.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2019 Contao Community Alliance.
+ * (c) 2013-2020 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -19,7 +19,7 @@
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright  2013-2019 Contao Community Alliance.
+ * @copyright  2013-2020 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -49,6 +49,8 @@ use ContaoCommunityAlliance\DcGeneral\Exception\DcGeneralRuntimeException;
  * Class ContaoWidgetManager.
  *
  * This class is responsible for creating widgets and processing data through them.
+ *
+ * @SuppressWarnings(PHPMD.LongClassName)
  */
 class ContaoWidgetManager
 {
