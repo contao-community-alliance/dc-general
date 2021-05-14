56eced1bdc7e1820b23ed11a4b12a88e92d99d2c
Ingolf Steinhardt <info@e-spin.de>
2021-05-14 14:18:06 +0200

    Fix readonly for filetree picker and item picker

--- src/Contao/View/Contao2BackendView/Widget/FileTree.php
+++ src/Contao/View/Contao2BackendView/Widget/FileTree.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2019 Contao Community Alliance.
+ * (c) 2013-2021 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2013-2019 Contao Community Alliance.
+ * @copyright  2013-2021 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -490,6 +490,7 @@ class FileTree extends AbstractWidget
             ->set('orderId', $this->orderId)
             ->set('link', $this->generateLink())
             ->set('label', $this->label)
+            ->set('readonly', $this->readonly)
             ->parse();
 
         return !Environment::get('isAjaxRequest') ? '<div>' . $content . '</div>' : $content;
