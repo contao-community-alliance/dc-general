3d30d9d8fb5c28ea9662c0dfe2de8e4e9dc6df55
Ingolf Steinhardt <info@e-spin.de>
2022-02-22 18:20:19 +0100

    Fix EventDispatcher

--- src/Event/AbstractEnvironmentAwareEvent.php
+++ src/Event/AbstractEnvironmentAwareEvent.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2019 Contao Community Alliance.
+ * (c) 2013-2022 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,8 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Tristan Lins <tristan.lins@bit3.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2013-2019 Contao Community Alliance.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2013-2022 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -23,12 +24,12 @@ namespace ContaoCommunityAlliance\DcGeneral\Event;
 
 use ContaoCommunityAlliance\DcGeneral\EnvironmentAwareInterface;
 use ContaoCommunityAlliance\DcGeneral\EnvironmentInterface;
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 /**
  * Abstract base class for environment aware events.
  */
-abstract class AbstractEnvironmentAwareEvent extends Event implements EnvironmentAwareInterface
+abstract class AbstractEnvironmentAwareEvent extends EventDispatcher implements EnvironmentAwareInterface
 {
     /**
      * The environment attached to this event.
