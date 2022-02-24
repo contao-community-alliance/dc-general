f2a6b8f7a6ebfb629f5737c03bd337a5d7414ae1
David Molineus <david.molineus@netzmacht.de>
2022-02-24 15:58:41 +0100

    Silence develop code warning

--- src/DataDefinition/Palette/Condition/Property/DumpingPropertyCondition.php
+++ src/DataDefinition/Palette/Condition/Property/DumpingPropertyCondition.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2019 Contao Community Alliance.
+ * (c) 2013-2022 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,8 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Tristan Lins <tristan.lins@bit3.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2013-2019 Contao Community Alliance.
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2013-2022 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -52,6 +53,8 @@ class DumpingPropertyCondition implements PropertyConditionInterface
 
     /**
      * {@inheritdoc}
+     *
+     * @SuppressWarnings (PHPMD.DevelopmentCodeFragment)
      */
     public function match(
         ModelInterface $model = null,
