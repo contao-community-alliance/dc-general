97f610f1d1d6cb55d480287419a603abb50c8bf1
Ingolf Steinhardt <git@e-spin.de>
2022-02-28 18:10:59 +0100

    Fix symfony deprecations

--- src/Factory/Event/CreateDcGeneralEvent.php
+++ src/Factory/Event/CreateDcGeneralEvent.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2019 Contao Community Alliance.
+ * (c) 2013-2022 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,8 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Tristan Lins <tristan.lins@bit3.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2013-2019 Contao Community Alliance.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2013-2022 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -22,7 +23,7 @@
 namespace ContaoCommunityAlliance\DcGeneral\Factory\Event;
 
 use ContaoCommunityAlliance\DcGeneral\DcGeneral;
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * This event is emitted when a DcGeneral instance has been created.
