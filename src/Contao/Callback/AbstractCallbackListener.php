5d9c4770f8f8dccc401d445e9468e5901976e628
Ingolf Steinhardt <git@e-spin.de>
2022-05-22 13:54:18 +0200

    Fix symfony

--- src/Contao/Callback/AbstractCallbackListener.php
+++ src/Contao/Callback/AbstractCallbackListener.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2019 Contao Community Alliance.
+ * (c) 2013-2022 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,8 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Tristan Lins <tristan.lins@bit3.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2013-2019 Contao Community Alliance.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2013-2022 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -98,7 +99,7 @@ abstract class AbstractCallbackListener
     /**
      * Retrieve the arguments for the callback.
      *
-     * @param \Symfony\Component\EventDispatcher\Event $event The event being emitted.
+     * @param \Symfony\Contracts\EventDispatcher\Event $event The event being emitted.
      *
      * @return array
      */
