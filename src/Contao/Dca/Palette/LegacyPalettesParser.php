5e643c7a90ec454654d20cabc8e47ca69fabdf11
Ingolf Steinhardt <info@e-spin.de>
2022-05-24 15:49:16 +0200

    Fix PHP 8 warnings

--- src/Contao/Dca/Palette/LegacyPalettesParser.php
+++ src/Contao/Dca/Palette/LegacyPalettesParser.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2019 Contao Community Alliance.
+ * (c) 2013-2022 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,7 +15,8 @@
  * @author     Tristan Lins <tristan.lins@bit3.de>
  * @author     David Maack <david.maack@arcor.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2013-2019 Contao Community Alliance.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2013-2022 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -181,13 +182,13 @@ class LegacyPalettesParser
             }
 
             // Fallback for incomplete palettes without legend,
-            // Create an empty legend.
+            // create an empty legend.
             if (!$legend) {
                 $name = 'unnamed';
                 if ($palette->hasLegend($name)) {
                     $legend = $palette->getLegend($name);
                 } else {
-                    $legend = new Legend($matches[1]);
+                    $legend = new Legend($matches[1] ?? null);
                     $palette->addLegend($legend);
                 }
             }
