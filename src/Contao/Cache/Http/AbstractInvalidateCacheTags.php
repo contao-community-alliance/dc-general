b28e02762a03ce99a868c8a3349ad9e8553bcd0c
Ingolf Steinhardt <info@e-spin.de>
2022-05-30 12:03:03 +0200

    Add author

--- src/Contao/Cache/Http/AbstractInvalidateCacheTags.php
+++ src/Contao/Cache/Http/AbstractInvalidateCacheTags.php
@@ -12,6 +12,7 @@
  *
  * @package    contao-community-alliance/dc-general
  * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @author     Ingolf Steinhardt <info@e-spin.de>
  * @copyright  2013-2020 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
