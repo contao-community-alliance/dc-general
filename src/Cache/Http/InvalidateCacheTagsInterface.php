bca78906ca401d50e38f9f1aba80445f0d5a0d40
Ingolf Steinhardt <info@e-spin.de>
2022-05-30 11:57:56 +0200

    Fix author

--- src/Cache/Http/InvalidateCacheTagsInterface.php
+++ src/Cache/Http/InvalidateCacheTagsInterface.php
@@ -11,7 +11,6 @@
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    contao-community-alliance/dc-general
- * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @copyright  2013-2022 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
