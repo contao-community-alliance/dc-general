02e41b3ea69690c67a86ce7a37dd0c81c7cf142e
Ingolf Steinhardt <info@e-spin.de>
2022-07-01 17:57:38 +0200

    Fix PHP8 warnings

--- src/Data/DefaultEditInformation.php
+++ src/Data/DefaultEditInformation.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2019 Contao Community Alliance.
+ * (c) 2013-2022 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,7 +12,8 @@
  *
  * @package    contao-community-alliance/dc-general
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2013-2019 Contao Community Alliance.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2013-2022 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -92,10 +93,15 @@ class DefaultEditInformation implements EditInformationInterface
             $this->modelErrors[$modelId->getSerialized()] = [];
         }
 
-        $this->modelErrors[$modelId->getSerialized()][$property->getName()] = \array_merge(
-            (array) $this->modelErrors[$modelId->getSerialized()][$property->getName()],
-            $error
-        );
+        if(isset($this->modelErrors[$modelId->getSerialized()][$property->getName()])){
+            $this->modelErrors[$modelId->getSerialized()][$property->getName()] = \array_merge(
+                (array) $this->modelErrors[$modelId->getSerialized()][$property->getName()],
+                $error
+            );
+        } else {
+            $this->modelErrors[$modelId->getSerialized()][$property->getName()] = $error;
+        }
+
     }
 
     /**
