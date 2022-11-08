b8eec4e0b6a545a67cab9773d35571f6ef7a4e41
Kim Wormer <info@kim-wormer.de>
2022-11-08 15:44:56 +0100

    Added me as an author

--- src/Controller/BackendTreeController.php
+++ src/Controller/BackendTreeController.php
@@ -13,6 +13,7 @@
  * @package    contao-community-alliance/dc-general
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @author     Kim Wormer <hallo@heartcodiert.de>
  * @copyright  2013-2019 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
