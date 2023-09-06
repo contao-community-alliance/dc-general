35a226f8dc1050e5a86d106491d4f88ffc108fab
Ingolf Steinhardt <git@e-spin.de>
2023-09-07 00:04:41 +0200

    Any fixes

--- src/Contao/View/Contao2BackendView/ActionHandler/MultipleHandler/PasteAllHandler.php
+++ src/Contao/View/Contao2BackendView/ActionHandler/MultipleHandler/PasteAllHandler.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2020 Contao Community Alliance.
+ * (c) 2013-2023 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,7 +12,8 @@
  *
  * @package    contao-community-alliance/dc-general
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2013-2020 Contao Community Alliance.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2013-2023 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -73,7 +74,10 @@ class PasteAllHandler
      */
     public function handleEvent(ActionEvent $event)
     {
-        if (!$this->getScopeDeterminator()->currentScopeIsBackend() || ('pasteAll' !== $event->getAction()->getName())) {
+        if (
+            !$this->getScopeDeterminator()->currentScopeIsBackend()
+            || ('pasteAll' !== $event->getAction()->getName())
+        ) {
             return;
         }
 
