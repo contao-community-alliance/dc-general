c4501d4b438c05164b381f785bf7033ac8e3c4c8
Ingolf Steinhardt <info@e-spin.de>
2026-01-03 20:02:16 +0100

    Fix xlabel callback

--- src/Contao/Callback/PropertyInputFieldGetXLabelCallbackListener.php
+++ src/Contao/Callback/PropertyInputFieldGetXLabelCallbackListener.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2023 Contao Community Alliance.
+ * (c) 2013-2026 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,7 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2013-2023 Contao Community Alliance.
+ * @copyright  2013-2026 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -53,7 +53,7 @@ class PropertyInputFieldGetXLabelCallbackListener extends AbstractReturningPrope
     {
         $widget = $event->getWidget();
         if ($widget instanceof Widget) {
-            $widget->wizard .= $value;
+            $widget->xlabel .= $value;
         }
     }
 }
