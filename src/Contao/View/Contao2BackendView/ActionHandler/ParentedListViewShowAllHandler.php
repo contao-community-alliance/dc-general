a1b2d6b23de548e6a95a851f7d8a92593ff2f5b0
Ingolf Steinhardt <info@e-spin.de>
2026-04-09 08:23:47 +0200

    Fix clipboard to add first item if manual sorting active

--- src/Contao/View/Contao2BackendView/ActionHandler/ParentedListViewShowAllHandler.php
+++ src/Contao/View/Contao2BackendView/ActionHandler/ParentedListViewShowAllHandler.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general.
  *
- * (c) 2013-2024 Contao Community Alliance.
+ * (c) 2013-2025 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2013-2024 Contao Community Alliance.
+ * @copyright  2013-2025 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -54,7 +54,6 @@ use ContaoCommunityAlliance\DcGeneral\EnvironmentInterface;
 use ContaoCommunityAlliance\DcGeneral\Exception\DcGeneralRuntimeException;
 use ContaoCommunityAlliance\DcGeneral\InputProviderInterface;
 use ContaoCommunityAlliance\DcGeneral\Panel\PanelContainerInterface;
-use ContaoCommunityAlliance\DcGeneral\View\ViewInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
@@ -596,7 +595,7 @@ class ParentedListViewShowAllHandler extends AbstractListShowAllHandler
             return null;
         }
 
-        if ($allowPasteTop = (bool) ViewHelpers::getManualSortingProperty($environment)) {
+        if (!($allowPasteTop = (bool) ViewHelpers::getManualSortingProperty($environment))) {
             $subFilter = new Filter();
             $subFilter->andActionIsNotIn([ItemInterface::COPY, ItemInterface::DEEP_COPY]);
             $subFilter->andParentIsNot(ModelId::fromModel($parentModel));
@@ -606,7 +605,7 @@ class ParentedListViewShowAllHandler extends AbstractListShowAllHandler
             assert(\is_string($dataProvider));
 
             $dataParentProvider = $basicDefinition->getParentDataProvider();
-            assert(is_string($dataParentProvider));
+            assert(\is_string($dataParentProvider));
 
             $filter = new Filter();
             $filter->andModelIsFromProvider($dataProvider);
